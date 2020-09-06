d4db63b0de00e28e90e5509e56cd011de1331dce
Jose Cordero Fuentealba <jocorderf@gmail.com>
2020-09-06 17:14:43 -0300

    Parse data

--- bitcoin.rb
+++ bitcoin.rb
@@ -21,9 +21,14 @@ puts data
 
 =begin
 
-Esto es lo que obtenemos, si analizamos la estructura vemos que es un hash con otros hashes anidados.
+Esto es lo que obtenemos, ojo con esto porque es un string, por lo tanto debemos parsear esta data.
 
 {"bpi":{"2020-08-07":11827.265,"2020-08-08":11568.5,"2020-08-09":11666.45,"2020-08-10":12010.28,"2020-08-11":11832.5,"2020-08-12":11342.01,"2020-08-13":11565.095,"2020-08-14":11759.5,"2020-08-16":11908.33,"2020-08-17":11856.5,"2020-08-18":12327.5,"2020-08-19":11918.5,"2020-08-23":11611.5,"2020-09-02":11795.7,"2020-09-03":11302.8875,"2020-09-04":10292.5983,"2020-09-05":10439.5},"disclaimer":"This data was produced from the CoinDesk Bitcoin Price Index. BPI value data returned as USD.","time":{"updated":"Sep 6, 2020 00:03:00 UTC","updatedISO":"2020-09-06T00:03:00+00:00"}}
 
 =end
+
+data = JSON.parse(data)
+puts data.class  #Vemos que ahora es un hash
+
+#Vamos a iterar dentro de este hash.
 
